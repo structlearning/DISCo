171e414ed8ea28117b1d70953cbe45c931b07f75
Pritish C <chakrabortypritish@gmail.com>
2025-09-06 19:26:32 +0530

    Add parallel option to MUVERA aug slurm script

--- scripts/slurm_muvera_common.sh
+++ scripts/slurm_muvera_common.sh
@@ -13,4 +13,4 @@ cd ~/CMUVERA_IR_ref
 
 # Run your command
 # python3 -m src.cmuvera data.dataset_name="scifact" overwrite_index=False index=False augment=True dbl_norm=True method="muvera" muvera.num_repetitions=20 muvera.num_simhash_projections=5 muvera.projection_dimension=20 muvera.final_projection_dimension=2560 lin_dim=128 muvera.half_embs=True muvera.type="BERT" muvera.compress=False embedder.mode="disk"
-python3 -m src.cmuvera data.dataset_name="$1" overwrite_index=False index=False augment=$2 dbl_norm=True method="muvera" muvera.num_repetitions=$3 muvera.num_simhash_projections=$4 muvera.projection_dimension=$5 muvera.final_projection_dimension=$6 lin_dim=$7 muvera.half_embs=$8 muvera.fresh_start=$9 muvera.type="BERT" muvera.compress=False embedder.mode="disk"
+python3 -m src.cmuvera data.dataset_name="$1" overwrite_index=False index=False augment=$2 dbl_norm=True method="muvera" muvera.num_repetitions=$3 muvera.num_simhash_projections=$4 muvera.projection_dimension=$5 muvera.final_projection_dimension=$6 lin_dim=$7 muvera.half_embs=$8 muvera.fresh_start=$9 muvera.type="BERT" muvera.compress=False embedder.mode="disk" muvera.parallel=$10
