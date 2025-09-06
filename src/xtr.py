ced74e59ebf17ef43182857d4b24b99202a37a68
Pritish C <chakrabortypritish@gmail.com>
2025-09-06 19:23:30 +0530

    XTR-WARP searcher implementation

---
+++ src/xtr.py
@@ -0,0 +1,194 @@
+import torch
+import logging
+import warnings
+from omegaconf import OmegaConf
+
+logger = logging.getLogger(__name__)
+
+from .dataloader import get_dataloader
+from .embedder import ColBERTEmbedder
+from .endtoend import BaseE2E
+from .colbert_embs import DummyQueryForColbert
+from .utils import save
+
+import sys
+import os
+from tqdm import tqdm
+
+import warp
+from warp.engine.utils.index_converter import convert_index
+
+
+## top k retriever via WARP. retrieve topk via WARP then use our score function to get the final scores
+class WarpBaseline(BaseE2E):
+    def __init__(self, config):
+        super().__init__(config)
+        self.config = config
+        self.dataloader = get_dataloader(self.config.data)
+        self.embedder = ColBERTEmbedder(config.embedder)
+        self.variety = self.config.embedder.mv_type
+        
+        self.mv_type = config.embedder.mv_type + "/norm" # Default COLBERT behaviour 
+        with warp.infra.Run().context(warp.infra.RunConfig(nranks=1, experiment=f"xtr_{self.dataloader.dataset_name}")):
+            colbert_config=warp.infra.ColBERTConfig(
+                nbits=self.config.colbert.nbits,
+                root="./xtr_beir_expts/",
+                dim=self.config.embedder.emb_dim
+            )
+
+    def index(self):
+        dataset_name = self.dataloader.dataset_name
+        corpus_tsv_filename, _ = self.dataloader.get_tsv()
+        nbits = self.config.colbert.nbits
+        index_name = f"xtr_nbits={nbits}.noaug"
+        with warp.infra.Run().context(warp.infra.RunConfig(nranks=1, experiment=f"{self.config.data.dataset_name}/{self.config.embedder.type}/{self.mv_type}")):
+            config = warp.infra.ColBERTConfig(
+                nbits=nbits,
+                root="./xtr_beir_expts/",
+                dim=self.config.embedder.emb_dim,
+                doc_maxlen=warp.engine.constants.DOC_MAXLEN,
+                index_bsize=128,
+                index_root=f"./experiments/{self.config.data.dataset_name}/BERT/{self.mv_type}/indexes"
+            )
+            # indexer = warp.Indexer(checkpoint="google/xtr-base-en", config=config)
+            indexer = warp.Indexer(checkpoint="bert-base-uncased", config=config)
+            indexer.index(name=index_name, collection=corpus_tsv_filename, overwrite=self.config.overwrite_index)
+        ## index needs to be converted into a warp index
+        convert_index(os.path.join(config.index_root, index_name))
+
+    def search(self,qembs,k):
+        queries = DummyQueryForColbert(len(qembs))
+        ranking = self.searcher._search_all_Q(queries, qembs, k).todict()
+
+        res = []
+        for i in range(len(qembs)):
+            ids, _, _ = zip(*ranking[i])
+            res.append(ids) 
+            
+        max_len = max([len(val) for val in res])
+        logger.info(f"Max length of retrieved ids comes out to: {max_len}. Average length of merged ids is {sum([len(val) for val in res])/len(res)}. Otherwise would have been {self.config.colbert_topk}")
+        ## TODO:tensorize -- just initialise the tensor to -1 then do torch.where ==-1 tensor[:,:1]
+        result_tensor = torch.zeros((len(qembs), max_len), dtype=torch.int64)
+        for i in range(len(qembs)):
+            current_set = list(res[i])
+            current_set.extend([current_set[0]] * (max_len - len(current_set)))
+            result_tensor[i] = torch.tensor(current_set)
+        
+        return result_tensor
+    
+    def run(self):
+        # Init searcher
+        dataset_name = self.dataloader.dataset_name
+        nbits = self.config.colbert.nbits
+        index_name = f"xtr_nbits={nbits}.noaug"
+        index_root = f"./experiments/{self.config.data.dataset_name}/BERT/{self.mv_type}/indexes"
+
+        # with warp.infra.Run().context(warp.infra.RunConfig(nranks=1, experiment=f"{self.config.data.dataset_name}/{self.config.embedder.type}/{self.mv_type}")):
+        #     config = warp.infra.ColBERTConfig(
+        #         nbits=nbits,
+        #         root="./xtr_beir_expts/",
+        #         dim=self.config.embedder.emb_dim,
+        #         doc_maxlen=warp.engine.constants.DOC_MAXLEN,
+        #         index_bsize=128,
+        #         index_root=index_root,
+        #     )
+
+        warp_config = warp.engine.config.WARPRunConfig(
+            nranks=4,
+            collection="beir", # beir or lotte
+            dataset=dataset_name,
+            type_="search", # irrelevant unless LoTTE
+            datasplit="test",
+            nbits=nbits,
+            k=self.config.k, # is this the same as the search k?
+            t_prime=None,
+            bound=128,
+            fused_ext=True,
+            index_root_=index_root,
+            index_name_=index_name,
+        )
+
+        self.searcher = warp.Searcher(
+            checkpoint="bert-base-uncased",
+            index=index_name,
+            index_root=index_root,
+            config=warp_config,
+            warp_engine=True,
+        )
+        result_file_path = f"./pickles/results/xtr_{self.variety}_{self.config.data.dataset_name}_k{self.config.k}{self.suffix}.pkl"
+
+        self.embedder.embed_full_dataset(self.dataloader, mode=self.config.embedder.mode)
+        qembs, qmasks = self.embedder.qembs, self.embedder.qmasks
+
+        result_ids = self.search(qembs.cpu(), k=self.config.k)
+
+        if self.config.embedder.mode =="mem":
+            ## compute real scores with opts
+            cembs, cmasks = self.embedder.get_corpus(result_ids)
+        else:
+            corpus = self.embedder.get_corpus(result_ids)
+            logger.info("All required documents are loaded")
+            cembs = []
+            cmasks = []
+            for q_id in tqdm(range(qembs.shape[0]), desc="Processing queries"):
+                cemb, cmask = corpus[
+                    (q_id * torch.ones(result_ids.shape[1], dtype=torch.long, device=corpus.device)),
+                    torch.arange(result_ids.shape[1], dtype=torch.long, device=corpus.device)
+                ]
+
+                cembs.append(cemb)
+                cmasks.append(cmask)
+            cembs = torch.stack(cembs)
+            cmasks = torch.stack(cmasks)
+
+        ## qembs: 300,64,128, qmasks: 300,64
+        ## cembs: 300,100,330,128, cmasks: 300,100,330
+        
+        ## qembs is already masked out, and zeroed out
+        dp = torch.einsum("abc,adec->adbe",qembs,cembs.to(qembs.device))
+        masked = torch.where(cmasks.bool().to(qembs.device).unsqueeze(2),dp,-10)
+        partials = torch.cummax(torch.amax(masked,dim=3),dim=1)[0]
+        result_scores = torch.sum(partials,dim=2)
+        
+        save((result_ids, result_scores), result_file_path)
+        return result_ids, result_scores
+
+
+if __name__=="__main__":
+    warnings.filterwarnings("ignore", category=FutureWarning)
+    config = OmegaConf.load("configs/colbert.yaml")
+    cliconfig = OmegaConf.from_cli()
+    
+    conf = OmegaConf.merge(config, cliconfig)
+    os.makedirs("logs/colbert", exist_ok=True)
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(process)d - %(message)s',
+        handlers=[
+            logging.FileHandler(f'logs/colbert/{conf.method}_{conf.data.dataset_name}_{conf.retriever.type}_pid:{os.getpid()}.log'),
+            logging.StreamHandler()
+        ]
+    )
+    logger.info(conf)
+
+    assert conf.method.startswith("xtr")
+    assert conf.augment == False
+    logging.info(f"Going to run XTR iid")
+    obj = WarpBaseline(conf)
+
+    if conf.index:
+        import time
+        start = time.time()
+        print("Starting Indexing")
+        obj.index()
+        end = time.time()
+        print("Done in ", end-start)
+    else:
+        import time
+        start = time.time()
+        print("Starting Run")
+        
+        obj.run()
+        end = time.time()
+        
+        print("Done in ", end-start)
