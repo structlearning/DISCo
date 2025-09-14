bb2c3c47996f9bc2d79d94d13b814e2bed359063
Pritish C <chakrabortypritish@gmail.com>
2025-09-15 04:05:29 +0530

    Move torch installation to separate requirements file

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,5 +6,14 @@ uv venv $VIRTUALENVS/muvera_$HOSTNAME --python 3.10
 source $VIRTUALENVS/muvera_$HOSTNAME/bin/activate
 
 uv pip install -r muvera_requirements_py3_10.txt --index-strategy unsafe-best-match
+
+# If HOSTNAME is boa or gnu, then install from muvera_requirements_torch_gnu.txt
+if [[ "$HOSTNAME" == "gnu" || "$HOSTNAME" == "boa" ]]
+then
+    uv pip install -r muvera_requirements_torch_gnu.txt --index-strategy unsafe-best-match
+else
+    uv pip install -r muvera_requirements_torch.txt --index-strategy unsafe-best-match
+fi
+
 uv pip install -e ./ColBERT
 uv pip install -e $SUBMODLIB
