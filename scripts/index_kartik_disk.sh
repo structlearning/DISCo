ab69839eaab40f29064c7bb49002db40c192d45d
Pritish C <chakrabortypritish@gmail.com>
2025-09-15 04:08:25 +0530

    Index script changes for lotte benchmark

--- scripts/index_kartik_disk.sh
+++ scripts/index_kartik_disk.sh
@@ -7,6 +7,8 @@ elif [ "$HOSTNAME" = "fox" ]; then
 	GPUS=(0 1 2 3 4 )  # For FOX
 elif [ "$HOSTNAME" = "dog" ]; then
     GPUS=(0 1 2 3 4 5)
+elif [ "$HOSTNAME" = "bee" ]; then
+    GPUS=(0 1 2 3)
 elif [ "$HOSTNAME" = "iitb-dgx2.iitb.ac.in" ]; then
     GPUS=(0 1 2 3 4 5 6 7)
 else
@@ -16,9 +18,13 @@ fi
 
 NUM_RH_AUGMENT=8
 DATASET_NAME="$1"
+LOADER="$2"
+QUERY="$3"
 
 python3 -m src.colbert_embs \
     data.dataset_name="$DATASET_NAME" \
+    data.loader_type=$LOADER \
+    data.query_type=$QUERY \
     embedder.mode="disk" \
     overwrite_index=True \
     index=True \
@@ -36,7 +42,7 @@ for ((i = 0; i < NUM_RH_AUGMENT; i++)); do
     # Do not separate line by line using slash even though it looks aesthetic.
     # rh_num does not get set if that is done.
     CUDA_VISIBLE_DEVICES=$GPU_ID \
-    python3 -m src.colbert_embs data.dataset_name=$DATASET_NAME embedder.mode="disk" overwrite_index=True index=True augment=True dbl_norm=True method="augmented" generate_new_rh=True rh_num=$i &
+    python3 -m src.colbert_embs data.loader_type=$LOADER data.query_type=$QUERY data.dataset_name=$DATASET_NAME embedder.mode="disk" overwrite_index=True index=True augment=True dbl_norm=True method="augmented" generate_new_rh=True rh_num=$i &
 
     # Double augmentation flag off below
     # CUDA_VISIBLE_DEVICES=$GPU_ID \
