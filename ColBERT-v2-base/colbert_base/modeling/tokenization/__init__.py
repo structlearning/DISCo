f86bc59fd7a28aba395014e31a264cd1f745beb8
Pritish C <chakrabortypritish@gmail.com>
2025-09-25 01:11:21 +0530

    First iteration of removing unneeded files and folders

--- ColBERT-v2-base/colbert_base/modeling/tokenization/__init__.py
+++
@@ -1,3 +0,0 @@
-from colbert_base.modeling.tokenization.query_tokenization import *
-from colbert_base.modeling.tokenization.doc_tokenization import *
-from colbert_base.modeling.tokenization.utils import tensorize_triples
