965ac5a7dfe9d2de33d3033fbee8d37385277052
Pritish C <chakrabortypritish@gmail.com>
2025-09-25 01:24:44 +0530

    Remove scripts

--- scripts/remaining_indices.sh
+++
@@ -1,9 +0,0 @@
-CUDA_VISIBLE_DEVICES=2 \
-    python3 -m src.colbert_embs \
-        data.dataset_name=scifact \
-        overwrite_index=True \
-        index=True \
-        augment=True \
-        dbl_norm=False \
-        method="augmented" \
-        rh_num=7 &
