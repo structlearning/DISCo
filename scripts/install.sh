2dfe4198805b9fecee76ed6e35e4f2211df25b8d
Pritish C <chakrabortypritish@gmail.com>
2025-09-25 01:29:33 +0530

    Installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
+# VIRTUALENVS is the directory where virtualenvs are stored.
+# SUBMODLIB points to where you have cloned the submodlib repository.
 echo "virtualenv directory path: $VIRTUALENVS"
 echo "submodlib library directory path: $SUBMODLIB"
 
-# If hostname contains prajna, then set virtualenv name to just muvera.
-if [[ "$HOSTNAME" == *"prajna"* ]];
-then
-    VENV_NAME="muvera"
-else
-    VENV_NAME="muvera_$HOSTNAME"
-fi
+# Set your virtualenv directory name here
+VENV_NAME="muvera"
 echo "virtualenv name: $VENV_NAME"
+
+# We use uv as our package manager.
 uv venv $VIRTUALENVS/$VENV_NAME --python 3.10
 source $VIRTUALENVS/$VENV_NAME/bin/activate
 
 uv pip install -r muvera_requirements_py3_10.txt --index-strategy unsafe-best-match
+uv pip install -r muvera_requirements_torch.txt --index-strategy unsafe-best-match
 
-# If HOSTNAME is boa or gnu, then install from muvera_requirements_torch_gnu.txt
-if [[ "$HOSTNAME" == "gnu" || "$HOSTNAME" == "boa" ]]
-then
-    uv pip install -r muvera_requirements_torch_gnu.txt --index-strategy unsafe-best-match
-else
-    uv pip install -r muvera_requirements_torch.txt --index-strategy unsafe-best-match
-fi
-
+# Install from ColBERT and submodlib directories.
+# Note that in the case of WARP, we only need to clone it .
 uv pip install -e ./ColBERT
 uv pip install -e $SUBMODLIB
